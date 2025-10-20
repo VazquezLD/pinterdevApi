a62e60e5348b7839e5686c20a91d6b5673dc1759
VazquezLD <davlucho86@gmail.com>
2025-10-20 18:43:44 -0300

    arreglo cors peticiones 2

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-import dotenv from 'dotenv'; 
-import { Server } from './models/Server';
-
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import { Server } from "./models/Server";
+import dotenv from "dotenv";
 
 dotenv.config();
-const server = new Server();
+const app = new Server().app;
 
-export default server.app;
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(express.json());
+
+export default app;
+
